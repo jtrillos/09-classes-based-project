a09971f6cea98c09d62128ef045c94720651aefa
Jaime M Trillos <jaimemanueltrillos@gmail.com>
2023-02-24 14:12:39 +0100

    adding error boundaries

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -4,6 +4,7 @@ import { Fragment, Component } from "react";
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 /*const DUMMY_USERS = [
   { id: "u1", name: "Max" },
@@ -47,7 +48,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
+        <ErrorBoundary>
         <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
